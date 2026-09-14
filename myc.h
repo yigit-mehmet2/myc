0c61f4946eec803181c321b7c7d69717b061773b
yigit-mehmet2 <yigitmehmetcintay2@gmail.com>
2026-09-14 21:54:50 +0300

    Add files via upload

---
+++ myc.h
@@ -0,0 +1,22 @@
+#ifndef MYC_H
+#define MYC_H
+
+// imports
+#include <stdio.h>
+#include <stdlib.h>
+
+// the C..
+#define NUMBER int 
+#define MAIN main
+#define BRACKET1 (
+#define BRACKET2 )
+#define CURLY1 {
+#define CURLY2 }
+#define WRITE printf
+#define GOBACK return
+#define ZERO 0
+#define SEMICOLON ;
+#define ALLOCATEMEMORY(byte) malloc(byte);
+#define GASTER void
+
+#endif
